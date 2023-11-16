f25caca0d218f594322f8544b1459842fe63261a
Pratap Singh <pratapsingh1729@gmail.com>
2023-11-16 01:38:56 -0500

    use blake2s for hkdf

--- wireguard-rs/src/wireguard/owl_wg/owl_hkdf.rs
+++ wireguard-rs/src/wireguard/owl_wg/owl_hkdf.rs
@@ -1,13 +1,13 @@
 use hkdf::Hkdf;
-use sha2::Sha256;
+use blake2::{Blake2s};
 use vstd::prelude::*;
 
 verus! {
 
 #[verifier(external_body)]
 pub fn extract_expand_to_len(ikm: &[u8], salt: &[u8], len: usize) -> Vec<u8> {
-    let h = Hkdf::<Sha256>::new(Some(salt), ikm);
-    let mut okm = vec![0u8; len];
+    let h = Hkdf::<Blake2s>::new(Some(salt), ikm);
+    let mut okm: Vec<u8> = vec![0u8; len];
     h.expand(&[], &mut okm).expect("failed to expand");
     okm
 }
