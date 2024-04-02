be3ddb4152249e5c7ce66e26d4974bfbea7ac93c
Pratap Singh <pratapsingh1729@gmail.com>
2024-04-02 13:57:47 -0400

    skeleton comment

--- src/Extraction/ExtractionTop.hs
+++ src/Extraction/ExtractionTop.hs
@@ -42,5 +42,15 @@ extract flags tcEnv path modbody = runExtractionMonad tcEnv (initEnv flags path)
 
 extract' :: TB.ModBody -> ExtractionMonad (OwlDoc, OwlDoc, OwlDoc)
 extract' modbody = do
+    {-
+    TODOS:
+    1.  Split apart the modbody into its components (locality map, shared names, public keys, etc). This
+        can reuse the preprocessing code from the bottom of Extraction.hs.old
+    2.  concretify, which generates CDef FormatTy, CStruct FormatTy, CEnum FormatTy
+    3.  lower to Rust types, using either `immut` or `opt`, generating CDef RustTy, CStruct RustTy, CEnum RustTy
+    4.  emit Rust: RustAST types
+    5.  just call owlpretty
+    6.  harness generation
+    -}
     throwError $ ErrSomethingFailed "TODO"
 
