25987ee539414e8d1ee81480a09ce8021c512133
Pratap Singh <pratapsingh1729@gmail.com>
2024-04-03 16:27:56 -0400

    switch back to Pretty instead of OwlPretty

--- src/Extraction/PrettyRust.hs
+++ src/Extraction/PrettyRust.hs
@@ -9,86 +9,85 @@ import Control.Monad.Trans
 import Prettyprinter
 import Data.Map.Strict as M
 import Rust
-import Pretty (OwlDoc, OwlPretty(..))
 
 
-instance OwlPretty BorrowKind where
-    owlpretty RMut = owlpretty "&mut "
-    owlpretty RShared = owlpretty "&"
+instance Pretty BorrowKind where
+    pretty RMut = pretty "&mut "
+    pretty RShared = pretty "&"
 
 
-instance OwlPretty RustTy where
-    owlpretty (RTRef bk ty) = owlpretty bk <> owlpretty ty
-    owlpretty (RTVec ty) = owlpretty "Vec" <> angles (owlpretty ty)
-    owlpretty (RTSlice ty) = owlpretty "[" <> owlpretty ty <> owlpretty "]"
-    owlpretty (RTArray ty n) = owlpretty "[" <> owlpretty ty <> owlpretty ";" <+> owlpretty n <> owlpretty "]"
-    owlpretty (RTTuple tys) = owlpretty "(" <> hsep (punctuate comma (fmap owlpretty tys)) <> owlpretty ")"
-    owlpretty (RTOption ty) = owlpretty "Option" <> angles (owlpretty ty)
-    owlpretty (RTNamed name) = owlpretty name
-    owlpretty RTUnit = owlpretty "()"
-    owlpretty RTBool = owlpretty "bool"
-    owlpretty RTU8 = owlpretty "u8"
-    owlpretty RTUsize = owlpretty "usize"
+instance Pretty RustTy where
+    pretty (RTRef bk ty) = pretty bk <> pretty ty
+    pretty (RTVec ty) = pretty "Vec" <> angles (pretty ty)
+    pretty (RTSlice ty) = pretty "[" <> pretty ty <> pretty "]"
+    pretty (RTArray ty n) = pretty "[" <> pretty ty <> pretty ";" <+> pretty n <> pretty "]"
+    pretty (RTTuple tys) = pretty "(" <> hsep (punctuate comma (fmap pretty tys)) <> pretty ")"
+    pretty (RTOption ty) = pretty "Option" <> angles (pretty ty)
+    pretty (RTNamed name) = pretty name
+    pretty RTUnit = pretty "()"
+    pretty RTBool = pretty "bool"
+    pretty RTU8 = pretty "u8"
+    pretty RTUsize = pretty "usize"
 
-owlprettyTyAnnot :: Maybe RustTy -> OwlDoc
-owlprettyTyAnnot Nothing = owlpretty ""
-owlprettyTyAnnot (Just ty) = colon <+> owlpretty ty
+prettyTyAnnot :: Maybe RustTy -> Doc ann
+prettyTyAnnot Nothing = pretty ""
+prettyTyAnnot (Just ty) = colon <+> pretty ty
 
-instance OwlPretty RustExpr where
+instance Pretty RustExpr where
     -- Special case for `let _ = ...;` where we omit the `let _ =`
-    owlpretty (RLet (False, "_", Nothing, expr) rest) = 
-        owlpretty expr <> semi <> line <> owlpretty rest
-    owlpretty (RLet (lt, name, ty, expr) rest) = 
-        owlpretty "let" <+> owlpretty (if lt then "mut " else "") <> owlpretty name <> owlprettyTyAnnot ty
-        <+> owlpretty "=" <+> owlpretty expr <> semi <> line <> owlpretty rest
-    owlpretty (RIfThenElse cond thenBranch elseBranch) =
-        owlpretty "if" <+> owlpretty cond <+> owlpretty "{" <> line <> 
-            owlpretty thenBranch <> line <> 
-        owlpretty "}" <+> owlpretty "else" <+> owlpretty "{" <> line <>
-            owlpretty elseBranch <> line <>
-        owlpretty "}"
-    owlpretty (RAssign name expr) = owlpretty name <+> owlpretty "=" <+> owlpretty expr <> semi <> line
-    owlpretty (RCall (name, args)) = owlpretty name <> parens (hsep (punctuate comma (fmap owlpretty args)))
-    owlpretty (RVar name) = owlpretty name
-    owlpretty (RBorrow (bk, expr)) = owlpretty bk <> owlpretty expr
-    owlpretty (RDeref expr) = owlpretty "*" <> owlpretty expr
-    owlpretty RUnit = owlpretty "()"
-    owlpretty (RUsizeLit n) = owlpretty n <> owlpretty "usize"
+    pretty (RLet (False, "_", Nothing, expr) rest) = 
+        pretty expr <> semi <> line <> pretty rest
+    pretty (RLet (lt, name, ty, expr) rest) = 
+        pretty "let" <+> pretty (if lt then "mut " else "") <> pretty name <> prettyTyAnnot ty
+        <+> pretty "=" <+> pretty expr <> semi <> line <> pretty rest
+    pretty (RIfThenElse cond thenBranch elseBranch) =
+        pretty "if" <+> pretty cond <+> pretty "{" <> line <> 
+            pretty thenBranch <> line <> 
+        pretty "}" <+> pretty "else" <+> pretty "{" <> line <>
+            pretty elseBranch <> line <>
+        pretty "}"
+    pretty (RAssign name expr) = pretty name <+> pretty "=" <+> pretty expr <> semi <> line
+    pretty (RCall (name, args)) = pretty name <> parens (hsep (punctuate comma (fmap pretty args)))
+    pretty (RVar name) = pretty name
+    pretty (RBorrow (bk, expr)) = pretty bk <> pretty expr
+    pretty (RDeref expr) = pretty "*" <> pretty expr
+    pretty RUnit = pretty "()"
+    pretty (RUsizeLit n) = pretty n <> pretty "usize"
 
-instance OwlPretty RustFunc where
-    owlpretty (RustFunc name args retTy body) = 
-        owlpretty "fn" <+> owlpretty name <> 
-        parens (hsep (punctuate comma (fmap (\(n, ty) -> owlpretty n <> colon <+> owlpretty ty) args)))
-        <+> owlpretty "->" <+> owlpretty retTy <+> owlpretty "{" <> line <>
-            indent 4 (owlpretty body) <> line <>
-        owlpretty "}"
+instance Pretty RustFunc where
+    pretty (RustFunc name args retTy body) = 
+        pretty "fn" <+> pretty name <> 
+        parens (hsep (punctuate comma (fmap (\(n, ty) -> pretty n <> colon <+> pretty ty) args)))
+        <+> pretty "->" <+> pretty retTy <+> pretty "{" <> line <>
+            indent 4 (pretty body) <> line <>
+        pretty "}"
 
-instance OwlPretty RustTyDecl where
-    owlpretty (RustTyDecl (name, ty)) = owlpretty "type" <+> owlpretty name <+> owlpretty "=" <+> owlpretty ty <> semi
+instance Pretty RustTyDecl where
+    pretty (RustTyDecl (name, ty)) = pretty "type" <+> pretty name <+> pretty "=" <+> pretty ty <> semi
 
-instance OwlPretty RustTraitImpl where
-    owlpretty (RustTraitImpl traitName forTy traitTys traitFuncs) = 
-        owlpretty "impl" <+> owlpretty traitName <+> owlpretty "for" <+> owlpretty forTy <+> owlpretty "{" <> line <>
-            indent 4 (vsep (fmap owlpretty traitTys)) <> line <>
-            indent 4 (vsep (fmap owlpretty traitFuncs)) <> line <>
-        owlpretty "}"
+instance Pretty RustTraitImpl where
+    pretty (RustTraitImpl traitName forTy traitTys traitFuncs) = 
+        pretty "impl" <+> pretty traitName <+> pretty "for" <+> pretty forTy <+> pretty "{" <> line <>
+            indent 4 (vsep (fmap pretty traitTys)) <> line <>
+            indent 4 (vsep (fmap pretty traitFuncs)) <> line <>
+        pretty "}"
 
-instance OwlPretty RustStructDecl where
-    owlpretty (RustStructDecl name fields implBlock traitImpls) = 
-        owlpretty "pub struct" <+> owlpretty name <+> owlpretty "{" <> line <>
-            indent 4 (vsep (fmap (\(n, ty) -> owlpretty n <> colon <+> owlpretty ty <> comma) fields)) <> line <>
-        owlpretty "}" <> line <> line <>
-        owlpretty "impl" <+> owlpretty name <+> owlpretty "{" <> line <>
-            indent 4 (vsep (fmap owlpretty implBlock)) <> line <>
-        owlpretty "}" <> line <> line <>
-        vsep (fmap owlpretty traitImpls)
+instance Pretty RustStructDecl where
+    pretty (RustStructDecl name fields implBlock traitImpls) = 
+        pretty "pub struct" <+> pretty name <+> pretty "{" <> line <>
+            indent 4 (vsep (fmap (\(n, ty) -> pretty n <> colon <+> pretty ty <> comma) fields)) <> line <>
+        pretty "}" <> line <> line <>
+        pretty "impl" <+> pretty name <+> pretty "{" <> line <>
+            indent 4 (vsep (fmap pretty implBlock)) <> line <>
+        pretty "}" <> line <> line <>
+        vsep (fmap pretty traitImpls)
 
-instance OwlPretty RustEnumDecl where
-    owlpretty (RustEnumDecl name variants implBlock traitImpls) = 
-        owlpretty "pub enum" <+> owlpretty name <+> owlpretty "{" <> line <>
-            indent 4 (vsep (fmap (\(n, ty) -> owlpretty n <> owlprettyTyAnnot ty <> comma) variants)) <> line <>
-        owlpretty "}" <> line <> line <>
-        owlpretty "impl" <+> owlpretty name <+> owlpretty "{" <> line <>
-            indent 4 (vsep (fmap owlpretty implBlock)) <> line <>
-        owlpretty "}" <> line <> line <>
-        vsep (fmap owlpretty traitImpls)
+instance Pretty RustEnumDecl where
+    pretty (RustEnumDecl name variants implBlock traitImpls) = 
+        pretty "pub enum" <+> pretty name <+> pretty "{" <> line <>
+            indent 4 (vsep (fmap (\(n, ty) -> pretty n <> prettyTyAnnot ty <> comma) variants)) <> line <>
+        pretty "}" <> line <> line <>
+        pretty "impl" <+> pretty name <+> pretty "{" <> line <>
+            indent 4 (vsep (fmap pretty implBlock)) <> line <>
+        pretty "}" <> line <> line <>
+        vsep (fmap pretty traitImpls)
