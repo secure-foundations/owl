3973d4b74379095264be6951a914a0ee3243a8ff
Pratap Singh <pratapsingh1729@gmail.com>
2025-06-18 02:08:06 -0400

    tweak

--- bench_wireguard_end_to_end.sh
+++ bench_wireguard_end_to_end.sh
@@ -2,8 +2,8 @@
 
 set -euxo pipefail
 
-python3 bench_wg_linedelay.py --bench_choice go
-python3 bench_wg_linedelay.py --bench_choice rs
-python3 bench_wg_mss.py --bench_choice go
-python3 bench_wg_mss.py --bench_choice rs
+python3 bench_wg_linedelay.py --bench-choice go
+python3 bench_wg_linedelay.py --bench-choice rs
+python3 bench_wg_mss.py --bench-choice go
+python3 bench_wg_mss.py --bench-choice rs
 
